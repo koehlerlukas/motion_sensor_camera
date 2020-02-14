0e56065a24c11e2f222194333f5a0ab155df67e1
Lukas Koehler <info@koehler-lukas.de>
2020-02-14 09:10:40 +0100

    Initial commit

--- motion_sensor_camera.py
+++ motion_sensor_camera.py
@@ -1,1 +1,22 @@
+import RPi.GPIO as GPIO
+import time
 
+PIR_ONE_PIN  = 23
+PIR2_TWO_PIN = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIR_ONE_PIN,  GPIO.IN)
+GPIO.setup(PIR2_TWO_PIN, GPIO.IN)
+
+def camera(channel):
+    # Take your photo here
+    print('Movement!')
+
+try:
+    GPIO.add_event_detect(PIR_ONE_PIN, GPIO.RISING, callback=camera)
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    print('KeyboardInterrupt detected.')
+
+GPIO.cleanup()
