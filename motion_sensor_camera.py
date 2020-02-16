876fe085550e6a8dd24006ffedc1125ea8a15bb1
Lukas Koehler <koehler.lukas997@gmail.com>
2020-02-16 22:36:41 +0100

    Files are now named after date+time, not shots taken

--- motion_sensor_camera.py
+++ motion_sensor_camera.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import picamera as PiCamera
 import time
 import argparse
+import datetime
 
 PROJECT_PATH = "/home/pi/Projekte/motion_sensor_camera/"
 PHOTO_PATH   = PROJECT_PATH + "photos/"
@@ -20,52 +21,24 @@ GPIO.setup(PIR_PIN_TWO, GPIO.IN)
 camera = PiCamera.PiCamera()
 #camera.rotation = 180 # Uncomment when the taken photo/video is upside down 
 
-# Format of shots.txt:
-#
-# x -> Number of photos taken
-# y -> Number of videos made
-
-def readNumbers():
-    with open("shots.txt", "r") as file:
-        return [int(x) for x in file.readlines()]
-
-def readNumber(photo):
-    if photo:
-        return readNumbers()[0]
-    else:
-        return readNumbers()[1]
-
-def updateNumber(photo):
-    numbers = readNumbers()
-    with open("shots.txt", "w+") as file:
-        if photo:
-            file.write(str(numbers[0] + 1) + "\n")
-            file.write(str(numbers[1]))
-        else:
-            file.write(str(numbers[0]) + "\n")
-            file.write(str(numbers[1] + 1))
-
-def readAndUpdateNumber(photo=True):
-    number = readNumber(photo)
-    updateNumber(photo)
-    return number
+def getCurrentDateTime():
+    now = datetime.datetime.now()
+    return now.strftime("%d.%m.%y-%X")
 
 def takePhoto(): # When callback add channel to arguments
     camera.start_preview()
-    photo = readAndUpdateNumber()
     time.sleep(2)
-    camera.capture(PHOTO_PATH + "photo" + str(photo) + ".jpg")
+    camera.capture(PHOTO_PATH + getCurrentDateTime() + ".jpg")
     camera.stop_preview()
-    print("Photo #" + str(photo) + " taken!")
+    print("Photo taken!")
 
 def makeVideo(): # When callback add channel to arguments
     camera.start_preview()
-    video = readAndUpdateNumber(photo=False)
-    camera.start_recording(VIDEO_PATH + "video" + str(video) + ".h264")
+    camera.start_recording(VIDEO_PATH + getCurrentDateTime() + ".h264")
     time.sleep(10)
     camera.stop_recording()
     camera.stop_preview()
-    print("Video #" + str(video) + " made!")
+    print("Video made!")
 
 def observe(mode):
     if not (mode == "video" or mode == "photo"):
